ebcebc9f39e43c73330cbb5495d0ac467c7524a7
Bradley Grzesiak <listrophy@gmail.com>
2009-07-24 23:30:59 -0500

    now looks for an updated rails gem

--- latest.rb
+++ latest.rb
@@ -36,3 +36,32 @@ def latest *args
   REQUIRES[package].each {|dependency| require dependency }
   SOFTWARE_LIST[package].call(args)
 end
+
+def latest_rails
+  log 'check', 'Checking for the latest version of rails'
+  gem_list = `gem list -r rails`
+  match = gem_list.match(/^\s*rails\s+\(([\d\.]*)\)(.)*/)
+  match.class.name == 'MatchData' ? match[1] : ''
+end
+
+def update_rails
+  rails_v = Rails::VERSION::STRING
+  latest_rails_v = latest_rails
+  if latest_rails_v == rails_v
+    log 'info', 'You already have the latest version of rails'
+  elsif latest_rails_v.blank?
+    log 'warning', 'Could not determine the latest version of rails. Skipping'
+  else
+    if ['y','Y','',"\n"].include?(ask("Your Rails gem version is #{rails_v}, but latest is #{latest_rails_v}. Update (Y/n)?"))
+      log 'update', 'Updating rails. This may take a few minutes, and it will require your admin password'
+      run "sudo gem install rails"
+      log 'remove', "Removing #{root}"
+      run "rm -rf #{root}"
+      log '**note**', 'Rails has been updated and your project has been deleted.'
+      log '**note**', 'Please re-run your previous command.'
+      exit
+    end
+  end
+end
+
+update_rails
